330faed99c1c38cda7e2f9935c4df56da8c21ec1
Stevens Cambranes <cambranesstevens@gmail.com>
2025-09-05 14:42:31 -0600

    Subiendo Capacitacion DLL

---
+++ ComboInteligente/CapaVista/Combo.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace CapaVista
+{
+    partial class Combo
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Cbo_Auto = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // Cbo_Auto
+            // 
+            this.Cbo_Auto.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Cbo_Auto.FormattingEnabled = true;
+            this.Cbo_Auto.Location = new System.Drawing.Point(0, 0);
+            this.Cbo_Auto.Name = "Cbo_Auto";
+            this.Cbo_Auto.Size = new System.Drawing.Size(245, 21);
+            this.Cbo_Auto.TabIndex = 0;
+            // 
+            // Combo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Cbo_Auto);
+            this.Name = "Combo";
+            this.Size = new System.Drawing.Size(245, 29);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox Cbo_Auto;
+    }
+}
---
+++ ComboInteligente/CapaVista/Combo.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaControlador;
+
+namespace CapaVista
+{
+    public partial class Combo : UserControl
+    {
+        public Combo()
+        {
+            InitializeComponent();
+        }
+
+        ///Vista///
+
+        Controlador cn = new Controlador();
+
+        public void llenarse(string tabla, string campo1, string campo2)
+        {
+
+            string tbl = tabla;
+            string cmp1 = campo1;
+            string cmp2 = campo2;
+
+
+
+            Cbo_Auto.ValueMember = "numero";
+            Cbo_Auto.DisplayMember = "nombre";
+
+            string[] items = cn.items(tabla, campo1, campo2);
+
+
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (items[i] != null)
+                {
+                    if (items[i] != "")
+                    {
+                        Cbo_Auto.Items.Add(items[i]);
+                    }
+                }
+
+            }
+
+            var dt2 = cn.enviar(tabla, campo1, campo2);
+            AutoCompleteStringCollection coleccion = new AutoCompleteStringCollection();
+            foreach (DataRow row in dt2.Rows)
+            {
+
+                coleccion.Add(Convert.ToString(row[campo1]) + "-" + Convert.ToString(row[campo2]));
+                coleccion.Add(Convert.ToString(row[campo2]) + "-" + Convert.ToString(row[campo1]));
+
+
+            }
+
+            Cbo_Auto.AutoCompleteCustomSource = coleccion;
+            Cbo_Auto.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            Cbo_Auto.AutoCompleteSource = AutoCompleteSource.CustomSource;
+
+
+        }
+    }
+}
